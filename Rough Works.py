2decd82552a14f383e55794c9c8c546c9dbdc528
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2021-11-03 15:37:13 +0400

    Add files via upload

---
+++ Rough Works.py
@@ -0,0 +1,187 @@
+#Q4
+'''import math 
+a = complex(input("Enter no"))
+b = complex(input("Enter no"))
+s = a+b
+print(complex(s))
+print(s.real,"is the real part")
+print(s.imag,"is imaginary part")'''
+
+'''#Q1
+N = int(input("Enter no of days")) 
+age = N*24*60*60
+print("age=",age)'''
+
+'''#Q5
+a = int(input("Enter side a=")) 
+b = int(input("Enter side b="))
+c = int(input("Enter side c="))
+
+S = (a+b+c)/2
+
+
+A = (S*(S-a)*(S-b)*(S-c))**0.5
+print(A)'''
+
+'''#Q2
+a = int(input("Enter qty in litres"))
+b = int(input("Enter qty in ml"))
+S = a+b
+print(S)
+print(S//1000, "litres")
+print(S%1000, "ml")'''
+
+'''#Q3
+x = int(input("Enter your number"))
+numbers = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+roman = ['I','IV','V','IX', 'X','XL', 'L','XC', 'C','CD', 'D','CM','M']
+i = 12  
+roman_numeral = ''
+while x != 0:
+    if numbers[i] <= x:    
+        roman_numeral += roman[i] 
+        x = x - numbers[i]
+    else:
+        i -= 1 # i = i - 1
+print(roman_numeral)'''
+
+
+'''a = 1;print("a value is",a);a=2;print(a)
+b = 1.234;print(b);
+print(format(b,".2f"));
+bool(1)
+bool(0)
+bool("abc")
+bool(False)
+bool("False")
+c=True;d="abcd"
+e=1+2j;f=2+3j;print(e,'\t',f);
+g=e+f;print("Real part of g is ",g.real,'\t',"Imaginary part of g is ",g.imag)
+p=2;q=4
+print(p,'\t',q)
+#tuple assignment
+p,q,r=1,2,3;print(p,'\t',q)
+#sequence assignment
+p,q="ab";print(p,'\t',q)
+#list assignment
+[p,q]=["ab","c"];print(p,'\t',q)'''
+
+'''a = 'apple'
+print(a)
+a,*b = 'All';print(a,'\t',b);
+a,b,*c = 'apple';print(a,'\t',b,'\t',c);
+[a,b,c] = ['a','pple','e'];print(a,'\t',b,'\t',c);
+a,b,c,d = range(1,5);
+print(a,'\t',b,'\t',c,'\t',d);
+a,b = range(1,5,2);
+print(a,'\t',b);
+a,b,c,d = range(2,10,2);
+print(a,'\t',b,'\t',c,'\t',d);
+a,b,c,d = range(2,9,2);
+print(a,'\t',b,'\t',c,'\t',d);
+a,b,c,d = range(8,1,-2);
+print(a,'\t',b,'\t',c,'\t',d);'''
+
+'''Dist=int(input("Enter distance"))
+if Dist<0:
+    print("Enter positive number only")
+elif Dist<=10:
+    bill = Dist*15
+    print(bill)
+elif Dist>10 and Dist<=100:
+    bill=(Dist-10)*8+10*15
+    print(bill)
+else:
+    bill=(Dist-90)*15+90*6
+    print(bill)'''
+
+'''M=float(input())
+
+if M==0:
+    print("Enter appropriate Mark")
+  
+ 
+if M>=80:
+    if M!=0:
+        
+        c=input()
+        if c=="T":
+            bonus=M+M*0.08
+            print(bonus)
+        elif c=="L":
+            bonus=M+M*0.06
+            print(bonus)
+
+if M>=60 and M<80:
+    if M!=0:
+        
+        c=input()
+        if c=="T":
+            bonus=M+M*0.06
+            print(bonus)
+        elif c=="L":
+            bonus=M+M*0.04
+            print(bonus)
+
+if M>=40 and M<60:
+    if M!=0:
+        
+        c=input()
+        if c=="T":
+            bonus=M+M*0.04
+            print(bonus)
+        elif c=="L":
+            bonus=M+M*0.02
+            print(bonus)
+if M<40:
+    if M!=0:
+        
+        c=input()
+        if c=="T":
+            bonus=M+M*0
+            print(bonus)
+        elif c=="L":
+            bonus=M+M*0
+            print(bonus)'''
+'''S='122'
+if S.isdigit():
+    Sum=int(S[0])+int(S[1])+int(S[2])
+    print(Sum)'''
+
+
+'''n=int(input())
+sum = 0
+S=0
+for digit in str(n): 
+    sum += int(digit)
+if sum<10:
+    print(sum)
+if sum>10:
+    for i in str(sum):
+        S+=int(i)
+    print(S)'''
+
+'''P=int(input())
+D=int(input())
+B=int(input())#1=yes 0=no
+if P<0 or D<0 or B<0:
+    print("Enter Only Positive Value Please!")
+if P==1 and B==1:
+    bill = P*3000*D + 500*D*P
+    print(bill)
+if P==1 and B==0:
+    bill = P*3000*D
+    print(bill)
+elif P%2==1 and B==1:
+    bill = ((P//2)*4000 + 3000)*D + 500*P*D 
+    print(bill)
+
+elif P%2==0 and B==0:
+    bill = ((P//2)*4000)*D
+    print(bill)'''
+
+'''year=int(input())
+if (year%400==0) or (year%100!=0) and (year%4==0):
+    print("leap year")
+else:
+    print("not leap year")'''
