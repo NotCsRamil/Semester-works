991eb075f0bb5317ae69074e589348df0b809c93
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2021-11-10 20:00:36 +0400

    Add files via upload

---
+++ Moodle programs/Patterns.py
@@ -0,0 +1,84 @@
+#*
+#**
+#***
+'''n=int(input())
+for i in range(1,n+1):
+    for st in range(1,i+1):       
+        print("*",end="")
+    for sp in range(1,n-i+1):
+        print("",end="")
+    print()'''
+
+#  *
+ #**
+#***
+'''n=int(input())
+for i in range(1,n+1):
+    for sp in range(1,n-i+1):
+        print(" ",end="")
+    for st in range(1,i+1):       
+        print("*",end="")
+    print()'''
+
+
+#***
+#**
+#*
+'''n=int(input())
+for i in range(1,n+1):
+    for st in range(1,n-i+2):       
+        print("*",end="")
+    for sp in range(1,i):
+        print("",end="")
+    print()'''
+
+
+#***
+ #**
+ # *
+'''n=int(input())
+for i in range(1,n+1):
+    for sp in range(1,i):
+        print(" ",end="")
+    for st in range(1,n-i+2):       
+        print("*",end="")
+    print()'''
+
+#*
+#**
+#***
+#***
+#**
+#*
+
+'''n=int(input())
+for i in range(1,n+1):
+    for st in range(1,i+1):       
+        print("*",end="")
+    for sp in range(1,n-i+1):
+        print("",end="")
+    print()
+for i in range(1,n+1):
+    for st in range(1,n-i+2):
+        print("*",end="")
+    for sp in range(1,i):
+        print("",end="")
+    print()'''
+ 
+        
+#bddddb
+#bbddbb
+#bbbbbb
+
+'''n=int(input())
+for i in range(1,n+1):
+    for b in range(1,i+1):
+        print("b",end="")
+    for d in range(1,n-i+1):
+        print('d',end='')
+    for d in range(1,n-i+1):
+        print("d",end="")
+    for b in range(1,i+1):
+        print('b',end='')
+    print()'''
+
