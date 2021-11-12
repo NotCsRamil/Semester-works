f98e0bf1d578f115a0b3d3fde74746604d6718b5
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2021-11-12 18:10:05 +0400

    Update List.py

--- Moodle programs/List.py
+++ Moodle programs/List.py
@@ -1,7 +1,23 @@
-L=[]
+#sum of all elements
+'''L=[]
 N=int(input())#size of list
 for i in range(N):
     a=int(input())
     L.append(a)
 print(L)
-print(sum(L))
+print(sum(L))'''
+
+#sum of Left and right elements
+'''L=[]
+S=int(input())#size
+for i in range(S):
+    a=int(input())
+    L.append(a)
+for i in range(1,len(L)):
+    left = L[:i]
+    right = L[i+1:]
+    if sum(left)==sum(right):
+        print(L.index(L[i]))
+    elif sum(left)!=sum(right):
+        S=0
+print(S)'''
