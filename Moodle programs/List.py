d715a80c22d5e1ee10b00cc152fe593d899d5cda
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2021-11-15 16:04:01 +0400

    Update List.py

--- Moodle programs/List.py
+++ Moodle programs/List.py
@@ -19,5 +19,33 @@ for i in range(1,len(L)):
     if sum(left)==sum(right):
         print(L.index(L[i]))
     elif sum(left)!=sum(right):
-        S=0
-print(S)'''
+        Sum=0
+print(Sum)'''
+#M4_CSQ3
+'''L=[]
+N=int(input())
+S=int(input())
+for i in range(N):
+    L1=[]
+    for j in range(S):
+        a=input()
+        L1.append(a)
+    L.append(L1)
+for i in range(len(L)):
+    L[i][2]=int(L[i][2])
+    L[i][4]=float(L[i][4])
+#print(L)
+
+
+
+k=int(input())#index
+v=input()#value
+L2=[]
+for i in range(len(L)):
+    if v==L[i][k]:
+        L2+=L[i][0:6]
+    if v.isdigit():
+        if int(v)==L[i][k]:
+            L2+=L[i][0:6]
+
+print(L2)'''
