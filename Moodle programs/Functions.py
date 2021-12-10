38b1ff06dcc32484b5224be80ba57b62e5ea8121
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2021-12-10 17:46:07 +0400

    Update Functions.py

--- Moodle programs/Functions.py
+++ Moodle programs/Functions.py
@@ -1,5 +1,5 @@
 #M6_CSQ1
-def strf(s1,s2):
+'''def strf(s1,s2):
     sp=int(input())
     if s2 not in s1:
         print("NotAvailable")
@@ -9,4 +9,18 @@ def strf(s1,s2):
         print(s1.find(s2)-sp)
 S1=input()
 S2=input()
-strf(S1,S2)
+strf(S1,S2)'''
+
+#M6_CSQ2
+'''L=input() #(35,94) inputted as a string
+L=L[1:len(L)-1].split(",") #["35", "94"]
+m=int(L[0])
+n=int(L[1])
+def C_R(heads,legs):
+    if legs%2!=0 or heads==0 or heads>legs:
+        print("No solution")
+    else:
+        r=(legs-2*heads)//2
+        c=heads-r
+    return r,c
+F=print(C_R(m,n))'''
