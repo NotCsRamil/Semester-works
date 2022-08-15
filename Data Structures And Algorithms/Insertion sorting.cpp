bc766a212d9e79cb579e8e0647167671c83b55f5
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2022-08-15 20:36:36 +0530

    Update Insertion sorting.cpp

--- Data Structures And Algorithms/Insertion sorting.cpp
+++ Data Structures And Algorithms/Insertion sorting.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 using namespace std;
-void swap(int *a, int *b){
-	int temp=*a;
-	*a=*b;
-	*b=temp;
-}
 int main(){
 	int arr[10];
-	int i,j;
+	int i,j,key;
 	int n;
-	int min;
 	cin>>n;
 	for(i=0;i<n;i++){
 		cin>>arr[i];
 	}
-	for (i=0; i<n-1; i++) 
-    { 
-        min = i;
-        for (j=i+1; j<n; j++) 
-        if (arr[j]<arr[min]) 
-            min=j; 
-  
-        swap(&arr[min], &arr[i]); 
-    }
+	for(i=1;i<n;i++){
+		key=arr[i];
+		j=i-1;
+		while(j>=0 && arr[j]>key){
+			arr[j+1]=arr[j];
+			j=j-1;
+		}
+		arr[j+1]=key;
+	}	
 	for(i=0;i<n;i++){
 		cout<<" "<<arr[i];
 	} 
