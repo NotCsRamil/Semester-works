5ef532e37db75c092bec9d44c5463969f281f261
NotCsRamil <72912419+NotCsRamil@users.noreply.github.com>
2022-09-18 15:23:04 +0530

    Update Linear Search.cpp

--- Data Structures And Algorithms/Linear Search.cpp
+++ Data Structures And Algorithms/Linear Search.cpp
@@ -1,20 +1,5 @@
 #include <iostream>
 using namespace std;
-int binarysearch(int arr[],int l,int h,int x){
-	while (l<=h){
-		int m=l+(h-1)/2;
-		if(arr[m]==x){
-			return m;
-		}
-		if(arr[m]<x){
-			return l=m+1;
-		}
-		else if (arr[m]>x){
-			return h=m-1;
-		}
-	}
-	return -1;
-}
 int main(){
 	int arr[10];
 	int x,i;
@@ -24,11 +9,13 @@ int main(){
 		cin>>arr[i];
 	}
 	cin>>x;
-	int res=binarysearch(arr,0,n,x);
-	if(res==-1){
-		cout<<"element not in array";
-	}
-	else{
-		cout<<x<<" is present at "<<res;
+	for(i=0;i<n;i++){
+		if(x==arr[i]){
+			cout<<"Element found at "<<i;
+		}
+		else{
+			cout<<"Element not found";
+		}
 	}
 }
+
